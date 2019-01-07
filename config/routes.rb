c2a3eea7878b1ae433c529f9255f96c82d1b4559
Pio Molina <vpio@comcast.net>
2019-01-07 11:51:54 -0500

    user can create and destroy sessions by log in and log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  resources :users
   root 'home#index'
+
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
